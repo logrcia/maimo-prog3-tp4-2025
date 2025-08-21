66777549ba741f217218756622dcbbe703216243
logrcia <lolagm04@gmail.com>
2025-08-21 19:58:36 -0300

    added styles to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,12 @@
 const Footer = () => {
   return (
     <div className="bg-gray-900 text-white">
-    <div className="mx-15 py-8 text-xl">
-      <p className="mb-6">
+    <div className="mx-15 py-10 text-xl flex flex-col items-center">
+      <p className="mb-6 font-bold">
         © 2025 folklore fanpage. All rights to Taylor Swift & Republic Records.
         This site is a fan-made project and not affiliated with Taylor Swift.
       </p>
-      <h2>Follow for more updates</h2>
+      <h2 className="mb-2">Follow for more updates</h2>
       <div className="flex">
         <div className="mr-4">instagram</div>
         <div className="mr-4">x</div>
