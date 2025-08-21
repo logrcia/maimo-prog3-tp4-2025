a73b131652735be7e140c5d0dee0cd241560fd4e
logrcia <lolagm04@gmail.com>
2025-08-21 17:17:35 -0300

    add footer

---
+++ src/components/Footer.js
@@ -0,0 +1,20 @@
+const Footer = () => {
+  return (
+    <div className="bg-gray-900 text-white">
+    <div className="mx-15 py-8 text-xl">
+      <p className="mb-6">
+        © 2025 folklore fanpage. All rights to Taylor Swift & Republic Records.
+        This site is a fan-made project and not affiliated with Taylor Swift.
+      </p>
+      <h2>Follow for more updates</h2>
+      <div className="flex">
+        <div className="mr-4">instagram</div>
+        <div className="mr-4">x</div>
+        <div>youtube</div>
+      </div>
+    </div>
+    </div>
+  );
+};
+
+export default Footer;
