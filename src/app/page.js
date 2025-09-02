23d858f05faa00bdedfa483ca90bab2015942889
avi <avrilmotta@gmail.com>
2025-09-02 18:42:37 -0300

    added store

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,14 @@
 import Navbar from "@/components/Navbar";
 import SectionMusic from "@/components/SectionMusic";
 import Footer from "@/components/Footer";
+import SectionStore from "@/components/SectionStore";
 
 export default function Home() {
   return (
     <div>
       <Navbar/>
       <SectionMusic/>
+      <SectionStore/>
       <Footer/>
     </div>
   );
---
+++ src/components/CardStore.js
@@ -0,0 +1,77 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+const CardStore = () => {
+  return (
+    <section className="grid grid-cols-12">
+    <article className="col-span-3 rounded-3xl p-3 flex-col justify-between h-[500px] mx-15">
+        <Image
+            src="/assets/merch.svg"
+            width={200}
+            height={200}
+            alt="folklore-cd"
+            className="flex justify-center"
+        />
+        <h4 className="text-mg mb-2 text-[#0a0a0a] font-bold min-h-[1rem] flex justify-right">lore ipsu</h4>
+        <h4 className="text-mg mb-2 text-[#0a0a0a] min-h-[1rem] flex justify-right">19,99</h4>
+    </article> 
+    <article className="col-span-3 rounded-3xl p-3 flex-col justify-between h-[500px] mx-15">
+        <Image
+            src="/assets/merch.svg"
+            width={200}
+            height={200}
+            alt="folklore-cd"
+            className="flex justify-center"
+        />
+        <h4 className="text-mg mb-2 text-[#0a0a0a] font-bold min-h-[1rem] flex justify-right">lore ipsu</h4>
+        <h4 className="text-mg mb-2 text-[#0a0a0a] min-h-[1rem] flex justify-right">19,99</h4>
+    </article> 
+    <article className="col-span-3 rounded-3xl p-3 flex-col justify-between h-[500px] mx-15">
+        <Image
+            src="/assets/merch.svg"
+            width={200}
+            height={200}
+            alt="folklore-cd"
+            className="flex justify-center"
+        />
+        <h4 className="text-mg mb-2 text-[#0a0a0a] font-bold min-h-[1rem] flex justify-right">lore ipsu</h4>
+        <h4 className="text-mg mb-2 text-[#0a0a0a] min-h-[1rem] flex justify-right">19,99</h4>
+    </article> 
+    <article className="col-span-3 rounded-3xl p-3 flex-col justify-between h-[500px] mx-15">
+        <Image
+            src="/assets/merch.svg"
+            width={200}
+            height={200}
+            alt="folklore-cd"
+            className="flex justify-center"
+        />
+        <h4 className="text-mg mb-2 text-[#0a0a0a] font-bold min-h-[1rem] flex justify-right">lore ipsu</h4>
+        <h4 className="text-mg mb-2 text-[#0a0a0a] min-h-[1rem] flex justify-right">19,99</h4>
+    </article> 
+    <article className="col-span-3 rounded-3xl p-3 flex-col justify-between h-[500px] mx-15">
+        <Image
+            src="/assets/merch.svg"
+            width={200}
+            height={200}
+            alt="folklore-cd"
+            className="flex justify-center"
+        />
+        <h4 className="text-mg mb-2 text-[#0a0a0a] font-bold min-h-[1rem] flex justify-right">lore ipsu</h4>
+        <h4 className="text-mg mb-2 text-[#0a0a0a] min-h-[1rem] flex justify-right">19,99</h4>
+    </article> 
+    <article className="col-span-3 rounded-3xl p-3 flex-col justify-between h-[500px] mx-15">
+        <Image
+            src="/assets/merch.svg"
+            width={200}
+            height={200}
+            alt="folklore-cd"
+            className="flex justify-center"
+        />
+        <h4 className="text-mg mb-2 text-[#0a0a0a] font-bold min-h-[1rem] flex justify-right">lore ipsu</h4>
+        <h4 className="text-mg mb-2 text-[#0a0a0a] min-h-[1rem] flex justify-right">19,99</h4>
+    </article>
+    </section>
+  )
+}
+
+export default CardStore
