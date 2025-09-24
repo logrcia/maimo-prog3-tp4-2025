a982a9e97565318266e32655c05715c7829e02b1
avi <avrilmotta@gmail.com>
2025-09-24 20:58:22 -0300

    added navbar

--- src/components/ProductContainer.js
+++ src/components/ProductContainer.js
@@ -6,6 +6,7 @@ const ProductContainer = ({id}) => {
   const [quantity, setQuantity] = useState(1);
   return (
     <div className="flex mx-15 mt-15 justify-center items-center">
+      <Navbar/>
       <div className="w-[400px] h-[400px] bg-neutral-600"></div>
       <div className="ml-10">
       <div className="mb-8">
