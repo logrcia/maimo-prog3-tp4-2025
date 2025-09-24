cdc6caefb275dc04dce0cd0ed5c6d314deb572ed
avi <avrilmotta@gmail.com>
2025-09-24 20:26:47 -0300

    fixed navbar and hero

--- src/app/page.js
+++ src/app/page.js
@@ -4,11 +4,13 @@ import Footer from "@/components/Footer";
 import SectionStore from "@/components/SectionStore";
 import Lyrics from "@/components/Lyrics";
 import Tracklist from "@/components/Tracklist";
+import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
     <div>
       <Navbar/>
+      <Hero/>
       <SectionMusic/>
       <SectionStore/>
       <Lyrics/>
---
+++ src/components/Hero.js
@@ -0,0 +1,21 @@
+import Navbar from "./Navbar";
+
+const Hero = () => {
+  return (
+    <div
+      style={{
+        backgroundImage:
+          "linear-gradient(to bottom, rgba(3,7,18,1), rgba(3,7,18,0)), url(/dummy-images/Folklore.webp)",
+      }}
+      className="relative h-screen bg-cover bg-no-repeat bg-bottom text-white"
+    >
+      <Navbar />
+      <div className="flex h-full flex-col justify-center items-center">
+        <h1 className="text-5xl font-bold">Welcome to the lakes</h1>
+        <h3 className="text-3xl">a website for the poets</h3>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
